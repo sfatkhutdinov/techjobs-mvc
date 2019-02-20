8d7a03ac538623218c743797d10c9fb085147d88
Stanislav <stanislav.fatkhutdinov@gmail.com>
2019-02-20 15:15:51 -0500

    Completed TODOs

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -1,9 +1,13 @@
 package org.launchcode.techjobsmvc.controllers;
 
 
+import org.launchcode.techjobsmvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 @Controller
 @RequestMapping("search")
@@ -15,6 +19,22 @@ public class SearchController {
         return "search";
     }
 
-    //TODO #1 - Create handler to process search request and display results
+
+    @RequestMapping(value = "results")
+    public String search(Model model, String searchType, String searchTerm){
+        model.addAttribute("columns", ListController.columnChoices);
+
+        if (searchType.equals("all")){
+            ArrayList<HashMap<String, String>> searchResults = JobData.findByValue(searchTerm);
+            model.addAttribute("searchResults", searchResults);
+            return "search";
+        }else {
+            ArrayList<HashMap<String, String>> searchResults = JobData.findByColumnAndValue(searchType, searchTerm);
+
+            model.addAttribute("searchResults", searchResults);
+
+            return "search";
+        }
+    }
 
 }
